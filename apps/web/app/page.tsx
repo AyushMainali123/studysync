3ec81d3784bf8dbe9b5a215451e388f2f651fcdf
AyushMainali123 <ayushmainali11@gmail.com>
2025-08-26 20:45:58 +0545

    chore: added placeholder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,23 +1,20 @@
 import { DBUser } from '@studysync/types';
-import { Client } from './_components/client/Client';
+// import { Client } from './_components/client/Client';
 
 export default async function Home() {
-  const usersRaw = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/all`,
-    { cache: 'no-store' }
-  );
-  const users = (await usersRaw.json()) as { users: DBUser[] };
+  const usersRaw = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+    cache: 'no-store',
+  });
+  const users = (await usersRaw.json()) as DBUser[];
   return (
     <div>
-      {users.users.map((user) => (
+      {users.map((user) => (
         <div key={user?.id}>
           <h2>{user?.name}</h2>
           <p>{user?.email}</p>
         </div>
       ))}
-      <div>
-        <Client />
-      </div>
+      <div>{/* <Client /> */}</div>
     </div>
   );
 }
